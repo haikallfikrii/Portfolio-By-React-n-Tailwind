31f419818adb333a00d6ddbbbc48839b0bb84f34
haikallfikrii <fikri789haikal@gmail.com>
2024-07-01 03:04:57 +0800

    repai for copy

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React from "react"
 
-export default function Footer () {
-    return <div>Footer</div>
+export default function Footer() {
+  return (
+    <div className="hidden p-5 text-center lg:block">
+    &copy; 2024 Developed By Muhamad Fikri Haikal
+    </div>
+  )
 }
-
